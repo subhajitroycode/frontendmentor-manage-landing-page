0112a284b3e9ab799fb6848878bb5146fa7b6920
Subhajit Roy <roysubhajit8697@gmail.com>
2023-05-15 20:56:27 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(12, 100%, 82%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+    variants: {
+      fill: ["hover", "focus"], // this line does the trick
+    },
+  },
+  plugins: [],
+};
